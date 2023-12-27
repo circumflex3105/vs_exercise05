7dd66f96b64da2c147bccee129dabb47acdf1745
geyerf <geyerfabian@icloud.com>
2023-12-27 16:47:39 +0100

    working

---
+++ FrueherkennungIF.java
@@ -0,0 +1,7 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface FrueherkennungIF extends Remote {
+    Bericht analysiere(Roentgenbild bild) throws RemoteException;
+}
+
